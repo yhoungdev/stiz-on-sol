eb89a457d643ba268cb88b5a72a403c698e6d246
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-10-22 02:52:42 +0100

    configured mobile application for frontend

---
+++ app/native/app/(tabs)/explore.tsx
@@ -0,0 +1,30 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Image, Platform } from 'react-native';
+
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { View } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+       Welcome to Stiz on Sol
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
